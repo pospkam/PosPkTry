de59d02f06e69207fc867defa48d1799a2f27983
PosPk <pospkam@users.noreply.github.com>
2026-04-25 06:24:46 +0000

    fix: map height 500px to match hero+AI combined height

--- components/homepage/HomeMapPreview.tsx
+++ components/homepage/HomeMapPreview.tsx
@@ -196,12 +196,12 @@ export function HomeMapPreview() {
       </div>
 
       {/* Map */}
-      <div className="relative" style={{ height: '320px' }}>
+      <div className="relative" style={{ height: '500px' }}>
         <LeafletMap
           center={[53.0444, 158.6483]}
           zoom={7}
           markers={markers}
-          height="320px"
+          height="500px"
           attribution={false}
         />
         {loading && (
